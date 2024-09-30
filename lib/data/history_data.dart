36ec11d7582d7948375c1471e2e7f91d1a9aba9f
Suyuful-Bitriq-Alqowi <bitriqsteam@gmail.com>
2024-09-30 21:11:38 +0700

    Feat:[BA]create-adapter

---
+++ lib/data/history_data.dart
@@ -0,0 +1,26 @@
+final List<Map<String, String>> historyMovies = [
+  {
+    'title': 'Deadpool 2016',
+    'studio': '20th Century Fox',
+    'imageUrl': 'images/dedpol.png',
+    'rating': '★ 4.25'
+  },
+  {
+    'title': 'Ponyo',
+    'studio': 'Studio Ghibli',
+    'imageUrl': 'images/ponyo.png',
+    'rating': '★ 4.3'
+  },
+  {
+    'title': 'Fireworks',
+    'studio': ' SHAFT',
+    'imageUrl': 'images/fire.png',
+    'rating': '★ 2.6'
+  },
+  {
+    'title': 'Bumblebee 2018',
+    'studio': 'Paramount Pictures',
+    'imageUrl': 'images/bum.png',
+    'rating': '★ 3.9'
+  },
+];
--- lib/menu/loginpage.dart
+++ lib/menu/loginpage.dart
@@ -1,18 +1,69 @@
+import 'package:eduvie_movie/widget/button.dart';
 import 'package:eduvie_movie/widget/color.dart';
+import 'package:eduvie_movie/widget/img.dart';
+import 'package:eduvie_movie/widget/richtext.dart';
 import 'package:eduvie_movie/widget/text.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: primaryBackground,
       appBar: AppBar(
-        title: Text("History Menu"),
+        automaticallyImplyLeading: false,
+        backgroundColor: primaryBackground,
+        elevation: 0,
+        title: Center(
+          child: EduvieRichText(),
+        ),
       ),
-      body: Center(
-        child: MyTxt(
-          text: "ini history",
-          textColor: primaryBackground,
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const MyImg(
+              imageUrl: 'images/kevin.png',
+              width: 365,
+              height: 420,
+              borderRadius: 0,
+            ),
+            const SizedBox(height: 20),
+            const MyTxt(
+              text:
+                  '" Watch your favorite films and series on one platform. Eduvie+ is always with you, anytime, anywhere. "',
+              fontSize: 16,
+              textColor: whiteColor,
+              fontWeight: FontWeight.w300,
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            MyButton(
+              buttonText: "Sign in",
+              buttonColor: reddColor,
+              onPressed: () {
+                Get.toNamed('/dashboard');
+              },
+              width: 270,
+              fontWeight: FontWeight.w700,
+            ),
+            const SizedBox(height: 10),
+            MyButton(
+              buttonText: "Continue with Google",
+              buttonColor: secondBackground,
+              onPressed: () {},
+              imageUrl: 'images/google.png',
+              width: 270,
+              fontWeight: FontWeight.w700,
+            ),
+          ],
         ),
       ),
     );
