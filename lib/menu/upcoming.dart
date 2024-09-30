c0f76803c2458010af68e9d1d2dc98f3a5e0f940
Suyuful-Bitriq-Alqowi <bitriqsteam@gmail.com>
2024-09-30 21:17:30 +0700

    Feat:[BA]create-upcoming-page

--- lib/menu/upcoming.dart
+++ lib/menu/upcoming.dart
@@ -1,4 +1,6 @@
+import 'package:eduvie_movie/data/upcoming_data.dart';
 import 'package:eduvie_movie/widget/color.dart';
+import 'package:eduvie_movie/widget/img.dart';
 import 'package:eduvie_movie/widget/text.dart';
 import 'package:flutter/material.dart';
 
@@ -6,13 +8,77 @@ class UpcomingMenu extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: primaryBackground,
       appBar: AppBar(
-        title: Text("History Menu"),
+        backgroundColor: secondBackground,
+        automaticallyImplyLeading: false,
+        title: Row(
+          children: [
+            CircleAvatar(
+              radius: 20,
+              backgroundImage: AssetImage('images/fotoprofile.png'),
+            ),
+            const SizedBox(width: 16),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                MyTxt(
+                  text: 'Hi, BiTriX',
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  textColor: whiteColor,
+                ),
+                MyTxt(
+                  text: 'Watch the movie all you want',
+                  fontSize: 14,
+                  textColor: Colors.grey,
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
-      body: Center(
-        child: MyTxt(
-          text: "ini history",
-          textColor: primaryBackground,
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: GridView.builder(
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            crossAxisSpacing: 2,
+            mainAxisSpacing: 2,
+            childAspectRatio: 0.85,
+          ),
+          itemCount: imageUrls.length,
+          itemBuilder: (context, index) {
+            return Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                // Gambar
+                MyImg(
+                  imageUrl: imageUrls[index],
+                  borderRadius: 12.0,
+                  width: 160,
+                  height: 220,
+                ),
+                const SizedBox(height: 4.0),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(
+                      Icons.access_time,
+                      color: Colors.grey,
+                      size: 16.0,
+                    ),
+                    MyTxt(
+                      text: upcomingDates[index],
+                      fontSize: 12.0,
+                      textColor: whiteColor,
+                      fontWeight: FontWeight.w300,
+                    ),
+                  ],
+                ),
+              ],
+            );
+          },
         ),
       ),
     );
